febe1855c7cadc5f5b77fc89388bbd91bf32c4d2
Kirill Stytsenko <kirill@styts.com>
2016-11-14 21:28:44 +0100

    realtive path in cronjob

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -1,2 +1,3 @@
+cd "$(dirname "$0")"
 wget -q "http://www.wienerlinien.at/ogd_realtime/monitor?rbl=3362&rbl=3359&rbl=3363&rbl=8682&rbl=4251&rbl=4277&rbl=3365&sender=lAlQnOt2p6D8HdvL" -O tmp.json > /dev/null
 cat tmp.json | python -m json.tool > ../public/data.json && rm tmp.json
